e4dd949382c9ba5f1fa62636999cd9690b7a23b5
Caleb Davenport <xcmdprompt@gmail.com>
2018-08-20 17:24:57 +0000

    swapi

--- swapi/api/urls.py
+++ swapi/api/urls.py
@@ -7,8 +7,6 @@ from api import views
 urlpatterns = [
     path('people-detail/', views.single_people),
     path('people-list/', views.list_people),
-
-    # actual views
     path('people/<int:people_id>/', views.people_detail_view),
     path('people/', views.people_list_view),
 ]
